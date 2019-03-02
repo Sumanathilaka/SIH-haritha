811995721384cca6b747e6750f43ec3a07bd6c2a
TGDK Sumanathilaka <37451505+Sumanathilaka@users.noreply.github.com>
2019-03-02 14:53:15 +0530

    Update home.php

--- home.php
+++ home.php
@@ -124,6 +124,22 @@ $image_id=$row['image_id'];
     
     <!-- Middle Column -->
     <div class="w3-col m7">
+        
+         <div class="w3-row-padding">
+        <div class="w3-col m12">
+          <div class="w3-card w3-round w3-white">
+            <div class="w3-container w3-padding">
+              
+               <form action="search.php" method="POST" enctype="multipart/form-data">
+              <input type="text" class="w3-border w3-padding" name="post" placeholder="Search" style="width: 88%">
+               <button type="submit"><i class="fa fa-search w3-border w3-padding "></i></button>
+              
+
+           </form>
+            </div>
+          </div>
+        </div>
+      </div>
     
       <div class="w3-row-padding">
         <div class="w3-col m12">
